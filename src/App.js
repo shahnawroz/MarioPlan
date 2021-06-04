7f9dcdf06e5d9f15eff30c68be351e8de495d4b5
shahnawroz <sh.nawrose@gmail.com>
2021-06-04 19:31:54 +0600

    form design done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Dashboard from "./component/dashboard/Dashboard";
 import NavBar from "./component/layout/NavBar";
 import ProjectDetails from "./component/projects/ProjectDetails";
+import SignIn from "./component/auth/SignIn"
+import SignUp from "./component/auth/SignUp";
+import CreateProject from "./component/projects/CreateProject";
 
 class App extends Component {
   render() {
@@ -13,6 +16,9 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Dashboard} />
             <Route path="/project/:id" component={ProjectDetails} />
+            <Route path='/signin' component={SignIn}/>
+            <Route path='/signup' component={SignUp}/>
+            <Route path='/create' component={CreateProject}/>
           </Switch>
         </div>
       </BrowserRouter>
--- src/component/layout/SignedInLink.js
+++ src/component/layout/SignedInLink.js
@@ -4,7 +4,7 @@ import {NavLink} from 'react-router-dom'
 const SignedInLink=()=>{
     return(
      <ul className="right">
-         <li><NavLink to='/'>New Project</NavLink></li>
+         <li><NavLink to='/create'>New Project</NavLink></li>
          <li><NavLink to='/'>Log out</NavLink></li>
          <li><NavLink to='/' className="btn btn-floating pink lighten-1">SN</NavLink></li>
      </ul>
