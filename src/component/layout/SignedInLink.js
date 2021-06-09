0356798dd5cb649e0f18aaf293927e8a1c40cadb
shahnawroz <sh.nawrose@gmail.com>
2021-06-10 05:01:39 +0600

    Login Logout From Firestore Done

--- src/component/layout/SignedInLink.js
+++ src/component/layout/SignedInLink.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { signOut } from '../../store/action/authAction'
 
-const SignedInLink=()=>{
-    return(
-     <ul className="right">
-         <li><NavLink to='/create'>New Project</NavLink></li>
-         <li><NavLink to='/'>Log out</NavLink></li>
-         <li><NavLink to='/' className="btn btn-floating pink lighten-1">SN</NavLink></li>
-     </ul>
-    )
+const SignedInLinks = (props) => {
+  return (
+    <div>
+      <ul className="right">
+        <li><NavLink to='/create'>New Project</NavLink></li>
+        <li><Link onClick={props.signOut}>Log Out</Link></li>
+        <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
+      </ul>
+    </div>
+  )
 }
-export default SignedInLink
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signOut: () => dispatch(signOut())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(SignedInLinks)
